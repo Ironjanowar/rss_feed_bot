42b2b079f329be00db3167345dbaf5d8ae0d94f7
Ironjanowar <ironjanowar@gmail.com>
2018-12-26 17:14:12 +0100

    Better use sets

--- lib/rss_feed_bot/redis.ex
+++ lib/rss_feed_bot/redis.ex
@@ -1,17 +1,17 @@
 defmodule RssFeedBot.Redis do
   def add_link(uid, link) do
-    Redix.pipeline(:redix, [["LPUSH", uid, link], ["LPUSH", "users", uid]])
+    Redix.pipeline(:redix, [["SADD", uid, link], ["SADD", "users", uid]])
   end
 
   def get_links(uid) do
-    Redix.command(:redix, ["LRANGE", uid, 0, -1])
+    Redix.command(:redix, ["SMEMBERS", uid])
   end
 
   def get_links!(uid) do
-    Redix.command!(:redix, ["LRANGE", uid, 0, -1])
+    Redix.command!(:redix, ["SMEMBERS", uid])
   end
 
   def get_all_users() do
-    Redix.command(:redix, ["LRANGE", "users", 0, -1])
+    Redix.command(:redix, ["SMEMBERS", "users"])
   end
 end
